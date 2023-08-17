9d7fb1ad0847401066edd43254fb23704d8ec3b1
christianonoh <onohchibyk@gmail.com>
2023-08-17 18:00:03 +0100

    Update course model to add validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  validates :name, :description, :photo, :price, :teacher, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100000 }
 end
+
