a0532b6946b72d6408c35a2fe8662eac0920d5f1
christianonoh <onohchibyk@gmail.com>
2023-08-17 17:59:34 +0100

    Update schema and seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Juanito Perez")
+Course.create(name: "Ruby on Rails", description: "Aprende Ruby on Rails desde cero a nivel profesional", price: 20.0, teacher: "Juanito Perez", user_id: 1)
