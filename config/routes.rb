15be68d53140c14185ccd12f4fed74c72f3c946e
Ellyboi <vitoesitemitope@gmail.com>
2023-08-21 16:30:52 +0100

    update routes to accommondate reservations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :courses
+      resources :courses do
+        resources :reservations
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
