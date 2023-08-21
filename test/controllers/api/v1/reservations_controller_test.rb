653c24aa10c313161c2c493ff93b8769ab6cb449
Ellyboi <vitoesitemitope@gmail.com>
2023-08-21 22:10:59 +0100

    fix the linters error

--- test/controllers/api/v1/reservations_controller_test.rb
+++ test/controllers/api/v1/reservations_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class Api::V1::ReservationsControllerTest < ActionDispatch::IntegrationTest
-  test "should create reservation" do
+  test 'should create reservation' do
     assert_difference('Reservation.count') do
       post api_v1_reservations_url, params: {
         reservation: {
-          name: "John Doe",
-          email: "john@example.com",
-          date: "2023-08-17T12:00:00Z"
+          name: 'John Doe',
+          email: 'john@example.com',
+          date: '2023-08-17T12:00:00Z'
         }
       }, as: :json
     end
@@ -15,4 +15,3 @@ class Api::V1::ReservationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :created
   end
 end
-
