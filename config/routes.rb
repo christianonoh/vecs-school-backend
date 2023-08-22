fbe9bfe18dab28c44d4762d3cc105e4c3373d26b
christianonoh <onohchibyk@gmail.com>
2023-08-22 05:51:22 +0100

    Create current_user endpoint and update route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'current_user/', to: 'current_user#index'
+      resources :users
+      resources :courses do
+        resources :reservations
+      end
+    end
+  end
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -8,16 +18,4 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  namespace :api do
-    namespace :v1 do
-      resources :users
-      resources :courses do
-        resources :reservations
-      end
-    end
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
