799fe3d96deece22c8572cbec689d5209f0ad158
christianonoh <onohchibyk@gmail.com>
2023-08-22 01:20:46 +0100

    Add JTI to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   has_many :reservations, dependent: :destroy
   has_many :courses, through: :reservations
 end
