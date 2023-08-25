db2ae077c4f5cf63ae772c3a747c77038e3a4243
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-08-25 14:52:30 -0500

    add course data to registrations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,8 @@ class Reservation < ApplicationRecord
   belongs_to :course
 
   validates :date, :city, presence: true
+
+  def as_json(options = {})
+    super(options.merge(include: :course))
+  end
 end
