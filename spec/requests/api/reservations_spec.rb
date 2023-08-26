5d32d2a6e42a14db9ecb5614b119e34f3c4a477e
Ellyboi <vitoesitemitope@gmail.com>
2023-08-26 18:37:02 +0100

    update the reservation file for documentation

--- spec/requests/api/reservations_spec.rb
+++ spec/requests/api/reservations_spec.rb
@@ -1,4 +1,34 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/reservations', type: :request do
+  path 'api/reservations' do
+    get 'Retrieve all reservations' do
+      tags 'Reservations'
+      security [Bearer: []]
+      produces 'application/json'
+      parameter name: :Authorization, in: :header, type: :string
+
+      response '200', 'Get reservations' do
+        schema type: :array,
+        items: {
+          type: :object,
+          properties: {
+            id: { type: :integer },
+            user_id: { type: :integer },
+            course_id: { type: :integer },
+            city: { type: string },
+            course: {
+              type: :object,
+              properties: {
+                id: { type: :integer },
+                name: { type: :string },
+                photo: { type: :string },
+                teacher: { type: :string },
+                user_id: { type: :integer},
+                description: { type: :string },
+                price: { type: :number, format: :float}
+              }
+            }
+          }
+        }
 end
